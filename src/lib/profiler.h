1cf91edf499836f3910f956b41ed343f506b7871
rbackman <rbackman@users.noreply.github.com>
2025-07-07 19:52:55 -0700

    Improve profiler output with fps and max timings

--- src/lib/profiler.h
+++ src/lib/profiler.h
@@ -7,6 +7,8 @@ struct ProfilerStats {
     uint32_t count = 0;
     uint64_t totalLoop = 0;
     uint64_t totalLED = 0;
+    uint32_t maxLoop = 0;
+    uint32_t maxLED = 0;
     unsigned long lastPrint = 0;
 };
 
@@ -14,21 +16,31 @@ static ProfilerStats profilerStats;
 
 inline void profilerAddLoop(uint32_t us) {
     profilerStats.totalLoop += us;
+    if (us > profilerStats.maxLoop) {
+        profilerStats.maxLoop = us;
+    }
     profilerStats.count++;
 }
 
 inline void profilerAddLed(uint32_t us) {
     profilerStats.totalLED += us;
+    if (us > profilerStats.maxLED) {
+        profilerStats.maxLED = us;
+    }
 }
 
 inline void profilerMaybePrint() {
     unsigned long now = millis();
     if (now - profilerStats.lastPrint >= 1000 && profilerStats.count > 0) {
-        Serial.printf("PROFILE avgLoop=%luus avgLED=%luus\n",
-                      (unsigned long)(profilerStats.totalLoop/profilerStats.count),
-                      (unsigned long)(profilerStats.totalLED/profilerStats.count));
+        Serial.printf("PROFILE fps=%lu avgLoop=%luus maxLoop=%luus avgLED=%luus maxLED=%luus\n",
+                      profilerStats.count,
+                      (unsigned long)(profilerStats.totalLoop / profilerStats.count),
+                      (unsigned long)profilerStats.maxLoop,
+                      (unsigned long)(profilerStats.totalLED / profilerStats.count),
+                      (unsigned long)profilerStats.maxLED);
         profilerStats.totalLoop = profilerStats.totalLED = 0;
         profilerStats.count = 0;
+        profilerStats.maxLoop = profilerStats.maxLED = 0;
         profilerStats.lastPrint = now;
     }
 }
