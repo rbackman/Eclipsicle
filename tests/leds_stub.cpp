437c828fa884ec3f90c0ab666b7741f0756ce4d4
rbackman <rbackman07@gmail.com>
2025-07-08 21:30:10 -0700

    Add sphere animation configuration and update parameters; refactor LED state name function

---
+++ tests/leds_stub.cpp
@@ -0,0 +1,16 @@
+#include "../src/shared.h"
+std::string getLedStateName(LED_STATE state)
+{
+    switch (state)
+    {
+    case LED_STATE_IDLE:
+        return "IDLE";
+    case LED_STATE_SINGLE_ANIMATION:
+        return "SINGLEANIMATION";
+    case LED_STATE_MULTI_ANIMATION:
+        return "MULTIANIMATION";
+    case LED_STATE_POINT_CONTROL:
+        return "POINTCONTROL";
+    }
+    return "UNKNOWN";
+}
