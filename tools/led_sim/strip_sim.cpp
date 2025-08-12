79dc8e38493d8045a84732eea8a6479fae7a0fd1
rbackman <rbackman@users.noreply.github.com>
2025-08-12 13:50:58 -0700

    Add LED state stub for host library

--- tools/led_sim/strip_sim.cpp
+++ tools/led_sim/strip_sim.cpp
@@ -3,6 +3,22 @@
 
 #include <string>
 
+std::string getLedStateName(LED_STATE state)
+{
+    switch (state)
+    {
+    case LED_STATE_IDLE:
+        return "IDLE";
+    case LED_STATE_SINGLE_ANIMATION:
+        return "SINGLEANIMATION";
+    case LED_STATE_MULTI_ANIMATION:
+        return "MULTIANIMATION";
+    case LED_STATE_POINT_CONTROL:
+        return "POINTCONTROL";
+    }
+    return "UNKNOWN";
+}
+
 extern "C" {
 
 StripState *stripsim_create(int led_count)
